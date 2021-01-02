09cca4feb90b59ec17faf4e049060edf16e4b712
Ria Gohel <ria.g@bluemeric.com>
2021-01-02 17:53:51 +0530

    Added trace_api for performing functions independent of rest api

--- cmd/sail.go
+++ cmd/sail.go
@@ -1,16 +1,19 @@
 package main
 
-import (
 
+import (
+	
 	"encoding/json"
 	"flag"
 	"fmt"
-	"log"
+	//"log"
+	//listProcess "gopaddle/sail/trace/listProcess"
+	trace "gopaddle/sail/trace"
 	"net/http"
 	"os"
 	"bytes"
 	"os/exec"
-	"io/ioutil"
+	//"io/ioutil"
 )
 
 func main() {
@@ -21,7 +24,7 @@ func main() {
 	listTextPtr := listCommand.String("all", "", "What to list (Required), list --all process") 
 	listHelpPtr := listCommand.Bool("help",false,"Help regarding commands")
 	dockerizeTextPtr := dockerizeCommand.String("pid", "", "pid of process (Required)")
-	dockerizeTimePtr := dockerizeCommand.String("time","2","Time for which trace commands runs")
+	dockerizeTimePtr := dockerizeCommand.Int("time",2,"Time for which trace commands runs")
 	dockerizeImagePtr := dockerizeCommand.String("imageName","final","Final image name")
 	dockerizeHelpPtr := dockerizeCommand.Bool("help",false,"Help regarding commands")
 
@@ -85,23 +88,12 @@ func main() {
 			dockerizeCommand.PrintDefaults()
 			os.Exit(1)
 		}
-
-		values := map[string]string{"time":*dockerizeTimePtr}
-		values2 := map[string]string{"osname":"ubuntu", "osver":"20.04"}
 		values4 := map[string]string{"finalimagename": *dockerizeImagePtr,"home": "/tmp"}
 		dir := [2]string{"packages.log", "pkg_install.sh"}
 		values3 := map[string][2]string{"dirs": dir}
-		jsonStr, _ := json.Marshal(values)
-		url := "http://localhost:9000/api/1/v1/startTracing?pid="+ *dockerizeTextPtr
-		url1 := "http://localhost:9000/api/1/v1/dockercreate"
 		url2 := "http://localhost:9000/api/1/v1/dockercopy"
-		url3 := "http://localhost:9000/api/1/v1/finalimage"
-
+		url3 := "http://localhost:9000/api/1/v1/finalimage"		
 		client := &http.Client{}
-		json1, err := json.Marshal(values2)
-		if err != nil {
-			panic(err)
-		}
 		json2, err3 := json.Marshal(values3)
 		if err3 != nil {
 			panic(err3)
@@ -110,44 +102,20 @@ func main() {
 		if err4 != nil {
 			panic(err4)
 		}		
-		request, err := http.NewRequest("PUT", url, bytes.NewBuffer(jsonStr))
-		response, err := client.Do(request)
-		if err != nil {
-			log.Fatal(err)
-		} else {
-			defer response.Body.Close()
-			_, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Print("Tracing pid = " + *dockerizeTextPtr)
-			fmt.Println("   ", response.StatusCode)
-			///////////////////////////////////////////////////////////////////////////////////
-			client1 := &http.Client{}
-			req, err := http.NewRequest("PUT", url1, bytes.NewBuffer(json1))
-			if err != nil {
-				fmt.Println(err)
-			}
-			req.Header.Set("Content-Type", "application/json; charset=utf-8")
-			resp, err := client1.Do(req)
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Print("Creating container for image = " + *dockerizeImagePtr)
-			fmt.Println("  ",resp.StatusCode)
-			////////////////////////////////////////////////////////////////////////////////////
+		trace.StartTracing_noreq(*dockerizeTextPtr,*dockerizeTimePtr)
+			osname := "ubuntu"
+			osver := "20.04"
+			trace.DockerCreate_noreq(osname, osver, "final")
 			req1, err1 := http.NewRequest(http.MethodPut, url2, bytes.NewBuffer(json2))
 			if err1 != nil {
 				fmt.Println(err1)
 			}
 			req1.Header.Set("Content-Type", "application/json; charset=utf-8")
-			resp, err2 := client.Do(req)
+			resp, err2 := client.Do(req1)
 			if err2 != nil {
 				fmt.Println(err2)
 			}
 			fmt.Print("Copying log file of trace to container...")
-			fmt.Println("   ",resp.StatusCode)
-			////////////////////////////////////////////////////////////////////////////////////
 			req3, err6 := http.NewRequest(http.MethodPut, url3, bytes.NewBuffer(json3))
 			if err6 != nil {
 				fmt.Println(err6)
@@ -160,11 +128,9 @@ func main() {
 			fmt.Print(*dockerizeImagePtr + " created")
 			fmt.Println("   ",resp.StatusCode)
 			fmt.Println("To check the image, use command : docker image inspect " + *dockerizeImagePtr)
-		}
 	}
 
 	if helpCommand.Parsed() {
-
 		fmt.Println("Enter sail [command] --help for more details on specific commands and their arguments.\nUse --[argument] for additional arguments")
 		fmt.Println("\nCommands")
 		fmt.Printf("%-12s%s\n","list","list processes")
